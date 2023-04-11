9b42a2211712a5468b79c8e35895ff363890d286
Asif Anam <anam1828@cseku.ac.bd>
2023-04-11 15:05:52 +0600

    last commit 11-4-23

--- script.js
+++ script.js
@@ -12,16 +12,29 @@ const btnNew = document.querySelector('.btn--new')
 const btnHold = document.querySelector('.btn--hold')
 const btnRoll = document.querySelector('.btn--roll')
 
+let scores =[0,0], currentScore= 0, activePlayer = 0, playing = true
+const init = function(){
+  const scores= [0,0]
+  let currentScore = 0
+  let activePlayer = 0
+  let playing = true
 
-score0El.textContent = 0
-score1El.textContent = 0
+  score0El.textContent = 0
+  score1El.textContent = 0
+  current0El.textContent = 0
+  current1El.textContent = 0
 
-const scores= [0,0]
-let currentScore = 0
-let activePlayer = 0
-let playing = true
+  diceEl.classList.add('hidden')
 
-diceEl.classList.add('hidden')
+  player0El.classList.remove('player--winner')
+  player1El.classList.remove('player--winner')
+  player0El.classList.add('player--active')
+  player1El.classList.remove('player--active')
+
+}
+
+init()
+
 
 const switchPlayer = function(){
   document.getElementById(`current--${activePlayer}`).textContent = 0
@@ -33,6 +46,8 @@ const switchPlayer = function(){
 //rolling dice functionality
 btnRoll.addEventListener('click', function(){
   //random dice roll
+  console.log('Clicked')
+  console.log(playing)
   if(playing) {
 
     const dice = Math.trunc(Math.random() * 6) + 1
@@ -49,7 +64,6 @@ btnRoll.addEventListener('click', function(){
     } else {
       //switch to next player
       switchPlayer()
-
     }
   }
 })
@@ -60,7 +74,7 @@ btnHold.addEventListener('click', function(){
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer]
     //check if player's score is >=100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       playing = false
       diceEl.classList.add('hidden')
 
@@ -74,3 +88,5 @@ btnHold.addEventListener('click', function(){
     //switch player
   }
 })
+
+btnNew.addEventListener('click', init)
